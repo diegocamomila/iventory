70661fd73866d91c15b0cbca8fe635a1b54fd860
diegocamomila <diego.camomila@gmail.com>
2022-11-01 13:41:47 -0300

    feat: 10 - Criar uma classe InventoryIterator, em  inventory_report/inventory/inventory_iterator.py

---
+++ inventory_report/inventory/inventory_refactor.py
@@ -0,0 +1,37 @@
+# obs: material de apoio descrito no arquivo zzzz.md
+from inventory_report.reports.complete_report import CompleteReport
+from inventory_report.reports.simple_report import SimpleReport
+from inventory_report.inventory.inventory_iterator import InventoryIterator
+import xmltodict as XmlImporter
+import csv as CsvImporter
+import json as JsonImporter
+
+
+class InventoryRefactor:
+    def __init__(self, importer):
+        self.importer = importer
+        self.newRport = []
+
+    def import_data(self, path, type):
+        self.newReport.extend(self.importer.import_data(path))
+        if ".csv" in path:
+            with open(path, encoding="utf-8") as file:
+                report = CsvImporter.DictReader(
+                    file, delimiter=",", quotechar='"'
+                )
+                newReport = list(report)
+        if ".xml" in path:
+            with open(path) as file:
+                report = XmlImporter.parse(file.read())
+                newReport = list(report["dataset"]["record"])
+        if ".json" in path:
+            with open(path) as file:
+                report = JsonImporter.load(file)
+                newReport = list(report)
+        if type == "simples":
+            return SimpleReport.generate(newReport)
+        else:
+            return CompleteReport.generate(newReport)
+
+    def __iter__(self):
+        return InventoryIterator(self.data)
