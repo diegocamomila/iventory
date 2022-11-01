3da2415c4659128afb7bc02bf87a0672c19f76f5
diegocamomila <diego.camomila@gmail.com>
2022-11-01 12:45:23 -0300

    feat: 9 - Testar a geração de uma versão do relatório em cores, tests/report_decorator/test_report_decorator.py

--- tests/report_decorator/test_report_decorator.py
+++ tests/report_decorator/test_report_decorator.py
@@ -1,6 +1,5 @@
-# from inventory_report.reports.colored_report import ColoredReport
-# from inventory_report.reports.simple_report import SimpleReport
-
+from inventory_report.reports.colored_report import ColoredReport
+from inventory_report.reports.simple_report import SimpleReport
 
 product = [
     {
@@ -16,4 +15,12 @@ product = [
 
 
 def test_decorar_relatorio():
-    pass  # Seu teste deve ser escrito aqui
+    # Seu teste deve ser escrito aqui
+    report = ColoredReport(SimpleReport).generate(product)
+    expected_response = (
+        "\x1b[32mData de fabricação mais antiga:"
+        "\x1b[0m \x1b[36m2022-05-04\x1b[0m\n\x1b[32mData de validade mais "
+        "próxima:\x1b[0m \x1b[36m2023-02-09\x1b[0m\n\x1b[32mEmpresa com mais "
+        "produtos:\x1b[0m \x1b[31mTarget Corporation\x1b[0m"
+    )
+    assert report == expected_response
